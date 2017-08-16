8b98b9126ee113a0fa373b85a0682ac365a9d9a5
HR_Zhang <jshori@163.com>
2017-08-16 18:37:55 +0800

    Delete comment

--- First_Python.py
+++ First_Python.py
@@ -1,4 +1,44 @@
 #!/usr/bin/python
+#-*- coding:utf-8 -*-
+#删除井号开头的行以及空行
+'''
+import os
+import re
+if (os.path.exists('beegfs-client.conf')):
+	os.system ('rm beegfs-client.conf')
+if (os.path.exists('beegfs-client.conf.bak')):
+	print "Bak File exist"
+	os.system('cp beegfs-client.conf.bak beegfs-client.conf')
+#else:
+#	os.system('cp beegfs-client.conf beegfs-client.conf.bak')
 
-list = [1,2,45]
-print list
+with open ('beegfs-client.conf','r') as f:
+	lines = f.readlines()
+with open ('beegfs-client.conf','w') as f_w:
+	for line in lines:
+		s = re.match("^#\s",line)
+		if s:
+			continue;
+#		f_w.write(line)
+		data = line.strip()
+		if len(data)!=0:
+			f_w.write(data)
+			f_w.write('\n')
+'''
+
+total = 0
+blank = 0
+pound = 0
+
+with open ('beegfs-client.conf','r') as f:
+	lines = f.readlines()
+	for line in lines:
+		total +=1
+		if not line.split():
+			blank +=1
+		line.strip()
+		if line.startswith('#'):
+			pound +=1
+	print 'Total line:',total
+	print 'Total blank',blank
+	print 'Total pound',pound
